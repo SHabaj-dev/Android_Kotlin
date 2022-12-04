c0ecbc7151e0324856735eb7d481afbb8c1a5053
SHabaj-dev <shabajansari84@gmail.com>
2022-12-04 15:42:05 +0530

    Completed Dice Roller APK

--- Dice Roller/app/src/main/java/com/sbz/diceroller/MainActivity.kt
+++ Dice Roller/app/src/main/java/com/sbz/diceroller/MainActivity.kt
@@ -2,10 +2,37 @@ package com.sbz.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.Random
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rollBtn : Button
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollBtn = findViewById(R.id.btnRoll)
+        diceImage = findViewById(R.id.ivDice)
+        rollBtn.setOnClickListener {
+            generateRandom()
+        }
+
+
+    }
+
+    private fun generateRandom() {
+        val selectImage = when(Random().nextInt(6) + 1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(selectImage)
     }
 }
