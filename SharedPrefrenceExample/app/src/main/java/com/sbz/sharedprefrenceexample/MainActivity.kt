bb93f724ea8f39346307d71c16b6d6ffab200c40
SHabaj-dev <shabajansari84@gmail.com>
2022-12-20 22:43:04 +0530

    Learned Shared Prefrences And TV and Buttons

--- SharedPrefrenceExample/app/src/main/java/com/sbz/sharedprefrenceexample/MainActivity.kt
+++ SharedPrefrenceExample/app/src/main/java/com/sbz/sharedprefrenceexample/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.sbz.sharedprefrenceexample
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sbz.sharedprefrenceexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sf: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        //Initializing Share Preference variable
+        sf = getSharedPreferences("my_sf", MODE_PRIVATE)
+        editor = sf.edit()
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val name = binding.etName.text.toString()
+        val age = binding.etAge.text.toString().toInt()
+
+        editor.apply {
+            putString("sf_name", name)
+            putInt("sf_age", age)
+            commit()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val name = sf.getString("sf_name", null)
+        val age = sf.getInt("sf_age", 0)
+
+        binding.etName.setText(name)
+
+        binding.etAge.setText(age.toString())
     }
 }
