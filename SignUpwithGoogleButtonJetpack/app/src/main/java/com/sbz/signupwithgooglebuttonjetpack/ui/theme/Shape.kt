5199bdcd1685094b4b9ea67bb367fe7241058561
SHabaj-dev <shabajansari84@gmail.com>
2023-02-13 17:38:25 +0530

    TextFields in Jetpack

--- SignUpwithGoogleButtonJetpack/app/src/main/java/com/sbz/signupwithgooglebuttonjetpack/ui/theme/Shape.kt
+++ SignUpwithGoogleButtonJetpack/app/src/main/java/com/sbz/signupwithgooglebuttonjetpack/ui/theme/Shape.kt
@@ -1,11 +1,11 @@
-package com.sbz.textfieldsjetpack.ui.theme
+package com.sbz.signupwithgooglebuttonjetpack.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
--- TextFieldsJetpack/app/src/main/java/com/sbz/textfieldsjetpack/MainActivity.kt
+++ TextFieldsJetpack/app/src/main/java/com/sbz/textfieldsjetpack/MainActivity.kt
@@ -3,11 +3,12 @@ package com.sbz.textfieldsjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sbz.textfieldsjetpack.ui.theme.TextFieldsJetpackTheme
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,14 +31,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember {
+            mutableStateOf("Enter Something...")
+        }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+//            readOnly = true,
+            label = {
+                Text(text = "Title")
+            },
+            singleLine = true,
+            shape = MaterialTheme.shapes.small
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TextFieldsJetpackTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
