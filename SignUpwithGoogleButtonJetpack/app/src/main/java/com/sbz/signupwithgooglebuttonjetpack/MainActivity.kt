cd9aef85561aebc90bc8423c839e8130822e5d3f
SHabaj-dev <shabajansari84@gmail.com>
2023-02-13 18:50:15 +0530

    Learnde Creating Custom Button

--- SignUpwithGoogleButtonJetpack/app/src/main/java/com/sbz/signupwithgooglebuttonjetpack/MainActivity.kt
+++ SignUpwithGoogleButtonJetpack/app/src/main/java/com/sbz/signupwithgooglebuttonjetpack/MainActivity.kt
@@ -1,43 +1,67 @@
 package com.sbz.signupwithgooglebuttonjetpack
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.sbz.signupwithgooglebuttonjetpack.ui.theme.SignUpWithGoogleButtonJetpackTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SignUpWithGoogleButtonJetpackTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    GoogleButton(
+                        text = "Sign Up With Google",
+                        loadingText = "Account Creating...",
+                        onClicked = {
+                            Toast.
+                            makeText(this@MainActivity, "Logging in.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    )
+
+                    GoogleButton(
+                        text = "Login With Mail",
+                        loadingText = "Please Weight",
+                        icon = painterResource(id = R.drawable.ic_launcher_foreground),
+                        onClicked = {
+                            Toast.
+                            makeText(this@MainActivity, "Logging in.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SignUpWithGoogleButtonJetpackTheme {
-        Greeting("Android")
+
     }
 }
